c5f25cd9c769103b3d9dfca17f776f2cf51c3181
H. Mineva <xpucu91@gmail.com>
2020-01-13 21:50:15 +0200

    Create dinamic page

---
+++ application.py
@@ -0,0 +1,26 @@
+import os
+import requests 
+import time
+
+from flask import Flask, render_template, request, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/posts", methods=["POST"])
+def posts():
+    start = int(request.form.get("start") or 0)
+    end = int(request.form.get("end") or (start + 9))
+
+    data = []
+    for i in range(start, end + 1):
+        data.append(f"Post #{i}")
+
+    time.sleep(1)
+
+    return jsonify(data)
